570c74074f8578d093d3bc69e4c8d86d720230fa
Michael Nadel <michael.nadel@gmail.com>
2020-02-04 22:03:29 -0600

    fixed the options screen (it wasn't a module so un-moduleified it)

--- options.js
+++ options.js
@@ -1,34 +1,28 @@
-class Options {
-    constructor(doc) {
-        doc.getElementById("options-save").addEventListener("click", () => {
-            const opts = {
-                url: doc.getElementById("options-url").value,
-                showBookmarks: doc.getElementById("options-show-bookmarks").checked,
-                weatherLat: doc.getElementById("options-lat").value,
-                weatherLon: doc.getElementById("options-lon").value,
-                darkSkyKey: doc.getElementById("options-darksky-key").value
-            }
-
-            options.save(opts, () => {
-                const status = doc.getElementById("options-status")
-                const currStatus = status.textContent
-
-                status.textContent = "Saved!"
-
-                setTimeout(() => {
-                    status.textContent = currStatus
-                }, 1000)
-            })
-        })
+document.getElementById("options-save").addEventListener("click", () => {
+    const opts = {
+        url: document.getElementById("options-url").value,
+        showBookmarks: document.getElementById("options-show-bookmarks").checked,
+        weatherLat: document.getElementById("options-lat").value,
+        weatherLon: document.getElementById("options-lon").value,
+        darkSkyKey: document.getElementById("options-darksky-key").value
     }
 
-    load = (doc, items) => {
-        doc.getElementById("options-url").value = items.url
-        doc.getElementById("options-show-bookmarks").checked = items.showBookmarks
-        doc.getElementById("options-lat").value = items.weatherLat
-        doc.getElementById("options-lon").value = items.weatherLon
-        doc.getElementById("options-darksky-key").value = items.darkSkyKey
-    }
-}
+    storage.save(opts, () => {
+        const status = document.getElementById("options-status")
+        const currStatus = status.textContent
 
-const options = new Options(document);
+        status.textContent = "Saved!"
+
+        setTimeout(() => {
+            status.textContent = currStatus
+        }, 1000)
+    })
+})
+
+storage.load((items) => {
+    document.getElementById("options-url").value = items.url
+    document.getElementById("options-show-bookmarks").checked = items.showBookmarks
+    document.getElementById("options-lat").value = items.weatherLat
+    document.getElementById("options-lon").value = items.weatherLon
+    document.getElementById("options-darksky-key").value = items.darkSkyKey
+})
